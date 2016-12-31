3963fa69121988e3db981cd5118cc1cb0fe44e6d
Harry Youd <youd44@gmail.com>
2016-12-31 17:37:05 +0000

    Begin creation of modular system

---
+++ settings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Settings
+  SourceTreeLoc="/home/harry/android/system"
+  DeviceList=(  "angler"
+                "deb"
+                "falcon" 
+                "flo"
+                "flounder" 
+                "flounder_lte" 
+                "huashan"
+                "klte"
+                "serranoltexx"  )
+  SyncOnStart=true
+  LogFileLoc="/home/harry/android/system/logs"
+  DeleteBuildAfterUpload=true
+  UseCcache=true
+  BuildTomorrow=true
