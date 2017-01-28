3c8214fd550db41ae8fac0adcb910d998e45419d
Harry Youd <harry@harryyoud.co.uk>
2017-01-28 19:39:26 +0000

    Raise!

--- settings.sh
+++ settings.sh
@@ -48,9 +48,9 @@
     #RepoTopics=( "rebrand" )
 
     # Override threads for building. This is set to CPU core count by default
-    # If this is too low (<2), you may run into weird problems like:
+    # If this is too low (<3), you may run into weird problems like:
     # javac: file not found: out/target/common/obj/APPS/org.cyanogenmod.platform-res_intermediates/src/cyanogenmod/platform/Manifest.java
-    #MakeThreadCount=2
+    #MakeThreadCount=3
 
 
 
