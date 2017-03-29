ce328573b46f2f5b55eba851fc3c3334b99a8dc3
Stricted <info@stricted.net>
2017-03-29 08:57:27 +0200

    use the right ota_from_target_files

--- functions.sh
+++ functions.sh
@@ -429,8 +429,9 @@ CleanupAfterBuild() {
 SignBuild() {
   if ! [[ -z $1 ]]; then
     OTAHash=$(ls -t $SourceTreeLoc/out/target/product/$1/obj/PACKAGING/target_files_intermediates | head -1 | sed -nr 's/lineage_'"$1"'-target_files-([0-9a-f]{10}).zip/\1/p')
+    OtaScriptPath=$(cat $SourceTreeLoc/out/target/product/$1/ota_script_path)
     LogCommandMake "build/tools/releasetools/sign_target_files_apks -o -d $SigningKeysPath $SourceTreeLoc/out/target/product/$1/obj/PACKAGING/target_files_intermediates/lineage_$1-target_files-$OTAHash.zip $SourceTreeLoc/out/target/product/$1/obj/PACKAGING/target_files_intermediates/lineage_$1-target_files-$OTAHash-signed.zip"
-    LogCommandMake "build/tools/releasetools/ota_from_target_files -k $SigningKeysPath/releasekey --block --backup=true $SourceTreeLoc/out/target/product/$1/obj/PACKAGING/target_files_intermediates/lineage_$1-target_files-$OTAHash-signed.zip $SourceTreeLoc/out/target/product/$1/lineage_$1-ota-$OTAHash.zip"
+    LogCommandMake "$OtaScriptPath -k $SigningKeysPath/releasekey --block --backup=true $SourceTreeLoc/out/target/product/$1/obj/PACKAGING/target_files_intermediates/lineage_$1-target_files-$OTAHash-signed.zip $SourceTreeLoc/out/target/product/$1/lineage_$1-ota-$OTAHash.zip"
     LogCommandMake "build/tools/releasetools/sign_zip.py -k $SigningKeysPath/releasekey $SourceTreeLoc/out/target/product/$1/lineage_$1-ota-$OTAHash.zip $SourceTreeLoc/out/target/product/$1/lineage_$1-ota-$OTAHash-signed.zip"
     LogCommandMake "rm $SourceTreeLoc/out/target/product/$1/lineage_$1-ota-$OTAHash.zip"
   else
